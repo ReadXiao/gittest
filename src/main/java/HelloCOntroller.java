c86151587b6107f0add9170fe8fd144246c5e027
ReadXiao <1392016499@qq.com>
2020-09-10 20:07:12 +0800

    第一次提交

---
+++ src/main/java/HelloCOntroller.java
@@ -0,0 +1,9 @@
+/**
+ * @author 肖宏武
+ * @date 2020/9/10 - 19:58
+ */
+public class HelloCOntroller {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
