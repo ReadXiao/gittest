1bf1afc0cb8c3f5d221cb6e1cf6680a2796fadd3
ReadXiao <1392016499@qq.com>
2020-09-10 20:11:08 +0800

    第二次提交

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -2,7 +2,7 @@
  * @author 肖宏武
  * @date 2020/9/10 - 19:58
  */
-public class HelloCOntroller {
+public class HelloController {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
